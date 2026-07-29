1a1923adc59b3cb8ee1f92f0b27529bcd602809d
suryarachana <suryarachana02@gmail.com>
2026-07-29 15:04:22 +0530

    Complete Day 6: Dashboard and AI Prediction APIs

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from app.database import engine, SessionLocal
 from app import models
-from app.schemas import UserCreate, UserLogin
+from app.schemas import UserCreate, UserLogin,DashboardResponse,PredictionRequest, PredictionResponse
 from app.auth import create_user, login_user,create_access_token,get_current_user
 from fastapi.security import OAuth2PasswordRequestForm
 
@@ -92,4 +92,43 @@ def get_profile(current_user: str = Depends(get_current_user)):
     return {
         "message": "Protected Route Accessed Successfully",
         "email": current_user
+    }
+
+
+@app.get("/dashboard", response_model=DashboardResponse)
+def get_dashboard():
+
+    return {
+        "health_score": 92,
+        "technical_debt_score": 18,
+        "total_projects": 5,
+        "last_scan": "2026-07-29"
+    }
+
+
+
+
+@app.post("/predict", response_model=PredictionResponse)
+def predict(request: PredictionRequest):
+
+    if request.lines_of_code <= 1000:
+        health_score = 95
+        technical_debt_score = 5
+        recommendation = "Excellent code quality"
+
+    elif request.lines_of_code <= 5000:
+        health_score = 85
+        technical_debt_score = 15
+        recommendation = "Good code quality"
+
+    else:
+        health_score = 70
+        technical_debt_score = 30
+        recommendation = "Needs refactoring"
+
+    return {
+        "project_name": request.project_name,
+        "health_score": health_score,
+        "technical_debt_score": technical_debt_score,
+        "recommendation": recommendation
     }
--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -7,3 +7,21 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):
     email:str 
     password:str 
+class DashboardResponse(BaseModel):
+    health_score: int
+    technical_debt_score: int
+    total_projects: int
+    last_scan: str
+
+
+
+class PredictionRequest(BaseModel):
+    project_name: str
+    lines_of_code: int
+
+
+class PredictionResponse(BaseModel):
+    project_name: str
+    health_score: int
+    technical_debt_score: int
+    recommendation: str
